583c17ee4e5db3d004dd4c4ec47e204cbad80ecc
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-07-27 00:41:10 +0300

    Updates

---
+++ precache-manifest.6df31c9e2e42b02ba9389b106884d6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a6de7d8c07ad35f391d8fe642cf81e",
+    "url": "/weather-page/index.html"
+  },
+  {
+    "revision": "6db15fe54cdb65d3eafb",
+    "url": "/weather-page/static/css/main.59d7af1a.chunk.css"
+  },
+  {
+    "revision": "eaefbe5779598d6f2f77",
+    "url": "/weather-page/static/js/2.945a3516.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-page/static/js/2.945a3516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db15fe54cdb65d3eafb",
+    "url": "/weather-page/static/js/main.8409034b.chunk.js"
+  },
+  {
+    "revision": "cea85ea1317e26e3159b",
+    "url": "/weather-page/static/js/runtime-main.a9ee8c47.js"
+  }
+]);
